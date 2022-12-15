d9db5a75fd956c13e633dfdf52b33dd427d82383
belie369 <katarzyna.derbich@gmail.com>
2022-12-15 13:03:19 +0100

    Added preventDefault for the form.

--- src/js/valid.js
+++ src/js/valid.js
@@ -1,6 +1,30 @@
-const prepareDOMElements = () => {};
+let firstName;
+let lastName;
+let mail;
+let phone;
+let message;
+let formBtn;
+let submitText;
 
-const prepareDOMEvents = () => {};
+const prepareDOMElements = () => {
+	firstName = document.querySelector('.form__input--first-name');
+	lastName = document.querySelector('.form__input--last-name');
+	mail = document.querySelector('.form__input--mail');
+	phone = document.querySelector('.form__input--phone');
+	message = document.querySelector('.form__message');
+	phone = document.querySelector('.form__input--phone');
+	formBtn = document.querySelector('.form__btn');
+	submitText = document.querySelector('form__submit-text');
+};
+
+const prepareDOMEvents = () => {
+	preventRefresh();
+};
+
+const preventRefresh = () =>
+	formBtn.addEventListener('click', (e) => {
+		e.preventDefault();
+	});
 
 prepareDOMElements();
 prepareDOMEvents();
